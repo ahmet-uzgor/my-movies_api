e738271d8709bee93e84e8aadf8be847c5987307
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-05-03 17:01:23 +0300

    GET a director with specified id and UPDATE a director information added

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const Director = require("../models/Director");
@@ -35,6 +36,48 @@ router.get('/',(req,res)=>{
 
     promise.then((director)=>{
         res.json(director);
+    }).catch((err)=>{
+        res.json({status: 404});
+    });
+});
+
+// api/director/:director_id GET director with a specified id,
+router.get('/:director_id',(req,res)=>{
+    const promise = Director.aggregate([
+    {
+        $match: {
+            '_id': mongoose.Types.ObjectId(req.params.director_id) 
+        }
+    },   
+    {
+        $lookup: {
+            from: 'movies',
+            localField: '_id',
+            foreignField: 'director_id',
+            as: 'movies'
+        }
+    },
+    {
+        $unwind:{
+            path: '$movies',
+            preserveNullAndEmptyArrays: true
+        }
+    }
+]);
+
+
+    promise.then((director)=>{
+        res.json(director);
+    }).catch((err)=>{
+        res.json({status: 404});
+    });
+});
+
+// api/directors PUT . it uptades a director information with specified id.
+router.put('/:director_id',(req,res)=>{
+    const promise = Director.findByIdAndUpdate(req.params.director_id);
+    promise.then((director)=>{
+        res.json({status: 1, uptadedDirector: director.name});
     }).catch((err)=>{
         res.json({status: 404});
     });
