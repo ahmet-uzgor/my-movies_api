a61bf3e5310b5a861c5383ec5b044576075a1d2e
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-05-03 17:27:49 +0300

    Validation of movie Schema is completed

--- models/Movie.js
+++ models/Movie.js
@@ -2,11 +2,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MovieSchema = new Schema({
-    title: {type: String , required: true,default:'ahmet'},
-    category: String,
+    title: {
+        type: String ,
+        required: [true,'`{PATH}` area is required.'],
+        maxlength: 50,
+        minlength: 1,
+        default:'ahmet'
+    },
+    category: {
+        type: String,
+        maxlength: 50,
+        minlength: 2
+    },
     country: String,
-    year: Number,
-    imdbScore: Number,
+    year: {
+        type: Number,
+        max:2020,
+        min: 1935
+    },
+    imdbScore: {
+        type: Number,
+        max: 10,
+        min: 1
+    },
     director_id: Schema.Types.ObjectId,
     dateOfMovie: {type: Date, default: Date.now}
 });
