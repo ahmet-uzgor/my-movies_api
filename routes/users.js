d88c22b5d1613ccc8b222d9fa9a4b3704971a98e
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-05-03 21:47:18 +0300

    users get post methos created

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require("../models/Users");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res)=> {
+  const promise = User.find({});
+  promise.then((users)=>{
+    res.json(users);
+  }).catch((err)=>{
+    res.json({status: 404});
+  });
+});
+
+router.post('/register', (req,res)=>{
+  const newUser = new User(req.body);
+  const promise = newUser.save();
+  promise.then((user)=>{
+    res.json({status: 1 , createdUser: user.username});
+  }).catch((err=>{
+    res.json({status: 404});
+  }));
 });
 
 module.exports = router;
