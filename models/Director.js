14af8fe5c857265e6a05704f2a55463e2eaf335a
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-05-03 17:31:39 +0300

    Validation of director Schema is completed

--- models/Director.js
+++ models/Director.js
@@ -2,9 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    name: String,
-    surname : String,
-    bio: String,
+    name: {
+        type: String,
+        maxlength: 70,
+        minlength: 2
+    },
+    surname : {
+        type: String,
+        maxlength: 30,
+        minlength: 1
+    },
+    bio: {
+        type: String,
+        maxlength: 500,
+        minlength:10
+    },
     createdDate: {
         type:Date,
         default: Date.now
