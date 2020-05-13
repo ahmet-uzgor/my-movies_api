060dd744b303b8b12fb613a39b1cfeae708f8c28
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-05-13 21:51:20 +0300

    api/movies POST method test is written

--- test/movie-test.js
+++ test/movie-test.js
@@ -29,5 +29,34 @@ describe('tests /api/movies', ()=>{
                     done();
                 });
         });
-    })
+    });
+
+    describe('/POST method tests /api/movies/',()=>{
+        it('it should post a movie with given body',(done)=>{
+            // post body
+            const testMovie = {
+                title: "Movie test",
+                director_id: "5eaeb7cd01bc0316f864b166",
+                category : "Action",
+                country : "America",
+                year : 2010,
+                imdbScore : 9.5
+            };
+            
+            chai.request(server)
+                .post('/api/movies')
+                .send(testMovie)
+                .set('x-access-token', token)
+                .end((err,res) =>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdbScore');
+                    done();
+                });
+        });
+    });
 });
